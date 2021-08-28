e0be8bcf8316928df35eb517a30fb6cd89726063
safixio <amirhasan.tester@gmail.com>
2021-08-28 15:19:23 +0430

    Transaction Pool Added

---
+++ Backend/SimpleBlockchain/SimpleBlockchain.Service/Implementations/TransactionPool.cs
@@ -0,0 +1,38 @@
+﻿using SimpleBlockchain.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace SimpleBlockchain.Service.Implementations
+{
+    public class TransactionPool : ITransactionPool
+    {
+        private List<Transaction> _transactions;
+        private object _object;
+
+        public TransactionPool()
+        {
+            _transactions = new List<Transaction>();
+            _object = new object();
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            lock (_object)
+            {
+                _transactions.Add(transaction);
+            }
+        }
+
+        public List<Transaction> GetTransactions()
+        {
+            lock(_object)
+            {
+                return _transactions;
+            }
+        }
+    }
+}
---
+++ Backend/SimpleBlockchain/SimpleBlockchain.Service/Interfaces/ITransactionPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace SimpleBlockchain.Service.Interfaces
+{
+    interface ITransactionPool
+    {
+        void AddTransaction(Transaction transaction);
+
+        List<Transaction> GetTransactions();
+    }
+}

7224700cb51bcb2d3a43a3f5ca034da5df130553
safixio <amirhasan.tester@gmail.com>
2021-08-28 15:02:51 +0430

    Transaction Model Added

---
+++ Backend/SimpleBlockchain/SimpleBlockchain.Model/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlockchain.Model
+{
+    public class Transaction
+    {
+        public string Amount { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}

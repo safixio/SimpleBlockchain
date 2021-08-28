d46c94303a84709a719418d888967ecf94aa5a80
safixio <amirhasan.tester@gmail.com>
2021-08-28 22:28:41 +0430

    BlockContainer Interface & Implementation Added

---
+++ Backend/SimpleBlockchain/SimpleBlockchain.Service/Implementations/BlockContainer.cs
@@ -0,0 +1,46 @@
+﻿using SimpleBlockchain.Model;
+using SimpleBlockchain.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlockchain.Service.Implementations
+{
+    public class BlockContainer : IBlockContainer
+    {
+        private List<Block> _blockLists;
+        private object _object;
+
+        public BlockContainer()
+        {
+            _blockLists = new List<Block>();
+            _object = new object();
+        }
+
+        public void AddBlock(Block block)
+        {
+            lock (_object)
+            {
+                _blockLists.Add(block);
+            }
+        }
+
+        public List<Block> GetAllBlocks()
+        {
+            lock (_object)
+            {
+                return _blockLists.ToList();
+            }
+        }
+
+        public Block GetLastBlock()
+        {
+            lock (_object)
+            {
+                return _blockLists.Last();
+            }
+        }
+    }
+}
---
+++ Backend/SimpleBlockchain/SimpleBlockchain.Service/Interfaces/IBlockContainer.cs
@@ -0,0 +1,18 @@
+﻿using SimpleBlockchain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlockchain.Service.Interfaces
+{
+    interface IBlockContainer
+    {
+        public void AddBlock(Block block);
+
+        public Block GetLastBlock();
+
+        public List<Block> GetAllBlocks();
+    }
+}

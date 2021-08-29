cbd688c510d8fc512da3a9605d446eea038803b9
safixio <amirhasan.tester@gmail.com>
2021-08-29 09:37:13 +0430

    HashService Interface & Implementation Added

---
+++ Backend/SimpleBlockchain/SimpleBlockchain.Service/Implementations/MerkleRootHash.cs
@@ -0,0 +1,60 @@
+﻿using SimpleBlockchain.Model;
+using SimpleBlockchain.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlockchain.Service.Implementations
+{
+    public class MerkleRootHash : IHashService
+    {
+        public string CalculateHash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                return ConvertBytesToHexadecimal(bytes);
+            }
+        }
+
+        public string CalculateHash(List<Transaction> transactions)
+        {
+            var transactionStrList = transactions.Select(tran => CalculateHash(CalculateHash(tran.From + tran.To + tran.Amount))).ToList();
+            return BuildMerkleRootHash(transactionStrList);
+        }
+
+        private string BuildMerkleRootHash(IList<string> merkelLeaves)
+        {
+            if (merkelLeaves == null || !merkelLeaves.Any())
+                return string.Empty;
+
+            if (merkelLeaves.Count() == 1)
+                return merkelLeaves.First();
+
+            if (merkelLeaves.Count() % 2 > 0)
+                merkelLeaves.Add(merkelLeaves.Last());
+
+            var merkleBranches = new List<string>();
+
+            for (int i = 0; i < merkelLeaves.Count(); i += 2)
+            {
+                var leafPair = string.Concat(merkelLeaves[i], merkelLeaves[i + 1]);
+                merkleBranches.Add(CalculateHash(CalculateHash(leafPair)));
+            }
+            return BuildMerkleRootHash(merkleBranches);
+        }
+
+        private string ConvertBytesToHexadecimal(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ Backend/SimpleBlockchain/SimpleBlockchain.Service/Interfaces/IHashService.cs
@@ -0,0 +1,15 @@
+﻿using SimpleBlockchain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlockchain.Service.Interfaces
+{
+    public interface IHashService
+    {
+        string CalculateHash(string rawData);
+        string CalculateHash(List<Transaction> transactions);
+    }
+}

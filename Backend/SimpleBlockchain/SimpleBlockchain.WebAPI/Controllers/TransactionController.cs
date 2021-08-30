d2c190be1e75ae81d461fea152e4c0971596819b
safixio <amirhasan.tester@gmail.com>
2021-08-30 11:21:06 +0430

    Transaction Controller Added

---
+++ Backend/SimpleBlockchain/SimpleBlockchain.WebAPI/Controllers/TransactionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleBlockchain.Model;
+using SimpleBlockchain.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBlockchain.WebAPI.Controllers
+{
+    [Route("api/v1/transactions")]
+    public class TransactionController : Controller
+    {
+        private readonly ITransactionPool _transactionPool;
+        public TransactionController(ITransactionPool transactionPool)
+        {
+            _transactionPool = transactionPool;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Transaction transaction)
+        {
+            _transactionPool.AddTransaction(transaction);
+            return Ok("Success");
+        }
+
+        [HttpPost("get-all")]
+        public IActionResult GetAll()
+        {
+            List<Transaction> transactions = _transactionPool.GetTransactions();
+            return Ok(transactions);
+        }
+    }
+}

fcb6ebd2849f1f2b33d7f6c2396e70c00fa94edb
safixio <amirhasan.tester@gmail.com>
2021-08-30 11:07:28 +0430

    Miner Controller Added

---
+++ Backend/SimpleBlockchain/SimpleBlockchain.WebAPI/Controllers/MinerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleBlockchain.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBlockchain.WebAPI.Controllers
+{
+    [Route("api/v1/miner")]
+    public class MinerController : Controller
+    {
+        private readonly IMiner _miner;
+
+        public MinerController(IMiner miner)
+        {
+            _miner = miner;
+        }
+
+        [HttpGet("start")]
+        public IActionResult Start()
+        {
+            _miner.Start();
+            return Ok("Success");
+        }
+
+        [HttpGet("stop")]
+        public IActionResult Stop()
+        {
+            _miner.Stop();
+            return Ok("Stop");
+        }
+    }
+}
